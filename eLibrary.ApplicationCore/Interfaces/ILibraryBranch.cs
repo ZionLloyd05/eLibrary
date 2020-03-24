9a3580905fd13dc33e8c20bafbc804463b20f78d
ZionLloyd05 <zionlloyd05@gmail.com>
2020-03-24 07:37:02 +0100

    UI changes

---
+++ eLibrary.ApplicationCore/Interfaces/ILibraryBranch.cs
@@ -0,0 +1,12 @@
+﻿using eLibrary.ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eLibrary.ApplicationCore.Interfaces
+{
+    public interface ILibraryBranch : IAsyncRepository<LibraryBranch>
+    {
+        IEnumerable<Patron> GetPatrons(int branchId);
+    }
+}
